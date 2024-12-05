b6e47aade331b90737e72e8f2790f739b0e06619
Gianmaria Saggini <gianmaria.saggini@gmail.com>
2024-12-05 11:02:51 +0100

    added java project

---
+++ myakiba/src/main/java/it/unipi/myakiba/MyakibaApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.myakiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyakibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyakibaApplication.class, args);
+	}
+
+}
---
+++ myakiba/src/test/java/it/unipi/myakiba/MyakibaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.myakiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyakibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
