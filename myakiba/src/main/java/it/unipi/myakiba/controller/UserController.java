1d4137430ab41681908a843b8ae78a6a179ef2e3
Gianmaria Saggini <gianmaria.saggini@gmail.com>
2024-12-07 10:41:40 +0100

    Setted gitignore

--- myakiba/src/main/java/it/unipi/myakiba/controller/UserController.java
+++ myakiba/src/main/java/it/unipi/myakiba/controller/UserController.java
@@ -1,4 +1,4 @@
-package it.unipi.myakiba.controllers;
+package it.unipi.myakiba.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
